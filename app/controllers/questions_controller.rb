10bc9b8555b174003ca6305219b45a297f5140a2
村上　瑛基 <ek.h1.hy@gmail.com>
2020-03-27 09:47:09 +0900

    クイズビューの変更

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,24 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.order("RAND()").limit(5)
-    @answers = Answer.order("RAND()").limit(4)
   end
+
+  # def new
+  #   @question = Question.new
+  #   @question.answers.build
+  # end
+  #
+  # def create
+  #   question = Question.new(question_params)
+  #   question.save
+  #   # 親要素を保存かけてあげることで自動で子要素も保存されます！
+  # end
+  #
+  # private
+  #
+  # def question_params
+  #   params.require(:user).permit(:question, answers_attributes: [:answer])
+  #   # 子要素のstrong parameterを書くことで、自動で子要素に親要素のidもふられます。この場合だと、answersテーブルにあるquestion_idにもきちんとこの時作成される親要素のidがふられる。
+  # end
+
 end
